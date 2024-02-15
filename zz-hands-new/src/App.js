b3472d6a3e9368f58df36ba7d59b1dbcddc0385d
Gabor Balint <trondheim.bg@gmail.com>
2024-02-15 16:26:23 +0100

    add planned components

--- zz-hands-new/src/App.js
+++ zz-hands-new/src/App.js
@@ -1,9 +1,27 @@
-
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Map from "./components/Map";
+import Navbar from "./components/Navbar";
+import Process from "./components/Process";
+import Services from "./components/Services";
+import Testimonials from "./components/Testimonials";
+import Why from "./components/Why";
 
 function App() {
   return (
     <>
-      
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Services/>
+      <Why/>
+      <Process/>
+      <Testimonials/>
+      <Contact/>
+      <Map/>
+      <Footer/>
     </>
   );
 }
