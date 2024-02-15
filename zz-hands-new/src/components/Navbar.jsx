d36c19ef0264aab8deedd3da4e053c47ecfbc2a5
Gabor Balint <trondheim.bg@gmail.com>
2024-02-15 16:40:53 +0100

    navbar + font

--- zz-hands-new/src/components/Navbar.jsx
+++ zz-hands-new/src/components/Navbar.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from "react";
+import Logo from "../images/zz_hands_original_logo.webp";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="navbar navbar-expand-lg bg-white fixed-top header-scroll">
+      <div className="d-flex container-fluid">
+        <a className="navbar-brand flex-grow-1 p-0" href="#header"><img src={Logo} alt="logo" /></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse flex-grow-0" id="navbarScroll">
+          <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll gap-2">
+            <li className="nav-item text-center">
+              <a className="nav-link active" href="#header">Home</a>
+            </li>
+            <li className="nav-item text-center">
+              <a className="nav-link" href="#services">Services</a>
+            </li>
+            <li className="nav-item text-center">
+              <a className="nav-link" href="#gallery">Portfolio</a>
+            </li>
+            <li className="nav-item text-center">
+              <a className="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
