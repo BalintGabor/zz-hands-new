ab34a88ea67e93ffc416c9838da1c21875d7cef9
Gabor Balint <trondheim.bg@gmail.com>
2024-02-15 20:35:45 +0100

    add footer component +css

--- zz-hands-new/src/components/Map.jsx
+++ zz-hands-new/src/components/Map.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Map() {
   return (
-    <div>Map</div>
+    <div id="map">
+      <div id="map-container-google-1" className="z-depth-1-half map-container">
+        <iframe src="https://maps.google.com/maps?q=london&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" title="map" allowfullscreen></iframe>
+      </div>
+    </div>
   )
 }
 
